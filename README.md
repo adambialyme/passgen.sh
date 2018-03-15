dad8a4d25455422bfac67c6a43519c2f18cce16b
adambialyme <adam.bialy.me@gmail.com>
2018-03-15 21:42:57 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Simple password generator with special character inside.
---
+++ genpass.sh
@@ -0,0 +1,33 @@
+f [ "${LENGTH}" == "" ]; then
+LENGTH="15"
+fi
+
+if [ "${LENGTH}" -le "7" ]; then
+LENGTH="8"
+echo "Passwd length needs to be at least 8 char"
+fi
+
+    MATRIX="0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+MATRIXS=".#-+"
+s=1
+while [ "${s:=1}" -le "2" ]
+do
+    PASSS="$PASSS${MATRIXS:$(($RANDOM%${#MATRIXS})):1}"
+    let s+=1
+done
+
+
+while [ "${n:=1}" -le "$((${LENGTH}-2))" ]
+do
+    PASS="$PASS${MATRIX:$(($RANDOM%${#MATRIX})):1}"
+    let n+=1
+
+	#add special character as 3rd and 4th
+	if [ "$n" = "3" ]; then
+	    PASS=${PASS}${PASSS}
+	fi
+
+done
+echo "$PASS"
+
